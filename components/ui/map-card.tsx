2360fc8acd380cedb68a882fd7179f13c7d127ae
Callum Alton <callumalton@PC11802.local>
2025-03-12 09:21:45 +0000

    add map icon

--- components/ui/map-card.tsx
+++ components/ui/map-card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { TitleCard } from "./title-card";
 import { map } from "@/assets";
+import { MapPinIcon } from "lucide-react";
 
 export const MapCard = async () => {
   return (
@@ -21,9 +22,12 @@ export const MapCard = async () => {
         />
       </div>
 
-      <div className="relative z-50 text-center h-full flex flex-col gap-2 py-4 bg-gradient-to-t from-white via-white/50 to-white/0 justify-end">
+      <div className="relative z-50 text-center h-full items-center flex flex-col gap-2 py-4 bg-gradient-to-t from-white via-white/50 to-white/0 justify-end">
+        <MapPinIcon className="stroke-white fill-red-600 h-8 w-8" />
         <span className="text-xl font-medium tracking-widest">MANCHESTER</span>
-        <span className="text-base font-normal tracking-wide opacity-50">SALFORD</span>
+        <span className="text-base font-normal tracking-wide opacity-50">
+          SALFORD
+        </span>
       </div>
     </TitleCard>
   );
